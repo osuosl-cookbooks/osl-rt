d0be95591e73fa035c568f5299f1c98956878881
Manatee <github@osuosl.org>
2025-01-30 17:44:19 +0000

    Automatic patch-level version bump to v1.0.2 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the osl-rt cookbook.
 
+1.0.2 (2025-01-30)
+------------------
+- Fixes for postfix-6.1.0
+
 1.0.1 (2024-07-05)
 ------------------
 - Test Kitchen Config Refactor
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ source_url        'https://github.com/osuosl-cookbooks/osl-rt'
 license           'Apache-2.0'
 chef_version      '>= 16.0'
 description       'Installs/Configures an instance of Request Tracker'
-version           '1.0.1'
+version           '1.0.2'
 
 depends           'certificate'
 depends           'osl-apache'
